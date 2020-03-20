6db6686186191aeb4d40746a45a3a4b733b1b134
hasiki <hasiki@nate.com>
2020-03-20 18:01:29 +0900

    JPA 테스트

---
+++ src/main/java/com/hasiki/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,29 @@
+package com.hasiki.springboot.web.dto;
+
+import com.hasiki.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
